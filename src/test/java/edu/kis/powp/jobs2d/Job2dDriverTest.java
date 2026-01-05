0bbe1f1ba2a14b8faa6b3adb79e0e9f65213d4a5
BeastRaecid <polskiarrow@protonmail.com>
2026-01-05 12:58:23 +0100

    ComplexCommand test was added to Job2dDriverTest.java file.

--- src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
+++ src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
@@ -3,6 +3,7 @@ package edu.kis.powp.jobs2d;
 import java.lang.management.OperatingSystemMXBean;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.commands.ComplexCommand;
 import edu.kis.powp.jobs2d.drivers.commands.OperateToCommand;
 import edu.kis.powp.jobs2d.drivers.commands.SetPositionCommand;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
@@ -16,6 +17,7 @@ public class Job2dDriverTest {
 	private static Job2dDriver driver = new StubDriver();
 	private static SetPositionCommand setPositionCommand;
 	private static OperateToCommand operateToCommand;
+	private static ComplexCommand complexCommand;
 
 	/**
 	 * Driver test.
@@ -28,18 +30,25 @@ public class Job2dDriverTest {
 		operateToCommand = new OperateToCommand(driver, 250, -300);
 		setPositionCommand.execute();
 		operateToCommand.execute();
+
+		// Test complex commands
+		complexCommand = new ComplexCommand();
+		complexCommand.addCommand(new SetPositionCommand(driver, 11111, 11111));
+		complexCommand.addCommand(new OperateToCommand(driver, -22222, -22222));
+		complexCommand.addCommand(new OperateToCommand(driver, 33333, 33333));
+		complexCommand.execute();
 	}
 
 	private static class StubDriver implements Job2dDriver {
 
 		@Override
 		public void operateTo(int x, int y) {
-			System.out.println("Driver operateTo action...");
+			System.out.println("Driver operateTo action... " + x + " " + y);
 		}
 
 		@Override
 		public void setPosition(int x, int y) {
-			System.out.println("Driver setPosition action...");
+			System.out.println("Driver setPosition action... " + x + " " + y);
 		}
 	};
 }
