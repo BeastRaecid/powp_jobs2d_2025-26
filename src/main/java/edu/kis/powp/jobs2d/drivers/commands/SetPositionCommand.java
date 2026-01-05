e1426c7d1e932aa413baf0e7bbdd3baf22b514bd
BeastRaecid <polskiarrow@protonmail.com>
2026-01-05 12:44:35 +0100

    New commands were implemented and tested.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/commands/SetPositionCommand.java
@@ -0,0 +1,19 @@
+package edu.kis.powp.jobs2d.drivers.commands;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+    private Job2dDriver driver;
+    private int x;
+    private int y;
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y) {
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void execute() {
+        this.driver.setPosition(x, y);
+    }
+}
--- src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
+++ src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
@@ -1,6 +1,10 @@
 package edu.kis.powp.jobs2d;
 
+import java.lang.management.OperatingSystemMXBean;
+
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.commands.OperateToCommand;
+import edu.kis.powp.jobs2d.drivers.commands.SetPositionCommand;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 /**
@@ -10,12 +14,20 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
  */
 public class Job2dDriverTest {
 	private static Job2dDriver driver = new StubDriver();
+	private static SetPositionCommand setPositionCommand;
+	private static OperateToCommand operateToCommand;
 
 	/**
 	 * Driver test.
 	 */
 	public static void main(String[] args) {
 		FiguresJoe.figureScript1(driver);
+
+		// Test commands
+		setPositionCommand = new SetPositionCommand(driver, 200, 200);
+		operateToCommand = new OperateToCommand(driver, 250, -300);
+		setPositionCommand.execute();
+		operateToCommand.execute();
 	}
 
 	private static class StubDriver implements Job2dDriver {
